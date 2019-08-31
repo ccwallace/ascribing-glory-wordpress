44671512f5f03c98023598b5d50d4026a9490cdd
cwallace <devcwallace@gmail.com>
2019-08-31 18:03:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,20 @@
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"  media="screen,projection"/>
 
+    <link rel="stylesheet" href="/css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
 
   <body>
-    <div class="col s12">
-      <img class="responsive-img col s12 ag-header-img" id="rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens" style="min-width: 500px;">
+    <div class="row">
+      <div class="col s12">
+        <img class="responsive-img" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens"></img>
+      </div>
+      <div class="col s6 push-s3">
+        <img class="responsive-img" src="http://34.94.0.6/wp-content/uploads/2019/08/ag-logo-transparent.png"></img>
     </div>
+  </div>
 
 
 
