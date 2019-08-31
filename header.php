b9759ef83a6853241f86d90b5d4e8bbe6e9bb738
cwallace <devcwallace@gmail.com>
2019-08-31 10:24:03 -0500

    Update header.php

--- header.php
+++ header.php
@@ -28,10 +28,7 @@
 </head>
 
 <body>
-
-		<div class="">
-			<img class="responsive-img" id="rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-nobg-e1567262239643.png" alt="Rainbow Pens Image">
-		</div>
+	<img class="responsive-img" id="rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-nobg-e1567262239643.png" alt="Rainbow Pens Image">
 
 	<?php
 	/**
