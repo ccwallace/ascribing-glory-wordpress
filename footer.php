b8e3e23eab0f8b729a413628b40d3dcbca381c63
cwallace <devcwallace@gmail.com>
2019-08-31 09:05:49 -0500

    Remove Storefront footer

--- footer.php
+++ footer.php
@@ -17,16 +17,6 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="col-full">
 
-			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' );
-			?>
-
 		</div><!-- .col-full -->
 	</footer><!-- #colophon -->
 
