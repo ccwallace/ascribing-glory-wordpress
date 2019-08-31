54ffd143b1483f2c9ce781e5b1d55bb0e1a14de0
cwallace <devcwallace@gmail.com>
2019-08-31 09:25:55 -0500

    Ascribing Glory Pens pic

--- header.php
+++ header.php
@@ -26,18 +26,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<header>
+		<img src="/assets/images/rainbow-pens-nobg.png" alt="">
 
-
-	  <nav>
-	    <div class="nav-wrapper">
-	      <a href="#" class="brand-logo">Logo</a>
-	      <ul id="nav-mobile" class="right hide-on-med-and-down">
-	        <li><a href="sass.html">Sass</a></li>
-	        <li><a href="badges.html">Components</a></li>
-	        <li><a href="collapsible.html">JavaScript</a></li>
-	      </ul>
-	    </div>
-	  </nav>
+	</header>
 
 	<?php
 	/**
