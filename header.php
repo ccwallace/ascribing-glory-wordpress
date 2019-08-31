5704d6572c7192090066896dfd507e81a794bee6
cwallace <devcwallace@gmail.com>
2019-08-31 10:25:32 -0500

    experiment without wp stuff

--- header.php
+++ header.php
@@ -30,16 +30,6 @@
 <body>
 	<img class="responsive-img" id="rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-nobg-e1567262239643.png" alt="Rainbow Pens Image">
 
-	<?php
-	/**
-	 * Functions hooked in to storefront_before_content
-	 *
-	 * @hooked storefront_header_widget_region - 10
-	 * @hooked woocommerce_breadcrumb - 10
-	 */
-	do_action( 'storefront_before_content' );
-	?>
-
 	<div id="content" class="site-content" tabindex="-1">
 		<div class="col-full">
 
