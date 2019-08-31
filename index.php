789d2e4c80a38068a7a06a1ac4170a88d7e0cfb3
cwallace <devcwallace@gmail.com>
2019-08-31 11:13:07 -0500

    add img

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
   </head>
 
   <body>
+    <img src="assets/img/rainbow-pens-nogb.png" alt="" class="col s12">
+
+
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
