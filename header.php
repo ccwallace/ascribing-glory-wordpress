b7ee36e00cfa949dd9853a24c82c7857727c4d0d
cwallace <devcwallace@gmail.com>
2019-09-05 16:27:59 -0500

    test grid nav

--- header.php
+++ header.php
@@ -29,6 +29,17 @@
 			<div class="col s12">
 				<img class="responsive-img rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens"></img>
 			</div>
+			<div class="col s1">
+				<nav class="transparent z-depth-0">
+					<a href="#" data-activates="mobile-demo" class="button-collapse fixed right"><i class="material-icons black-text">menu</i></a>
+				</nav>
+			</div>
+			<div class="col s2">
+				<a class="black-text material-icons main-mobile-menu-btn" href="/cart">shopping_cart</a>
+			</div>
+
+
+<!--
 			<nav class="transparent z-depth-0">
 				<div class="row">
 					<div class="col s2">
@@ -39,6 +50,8 @@
 					</div>
 				</div>
 			</nav>
+-->
+
 			<div class="col s6 push-s3">
 				<a href="/home">
 					<img class="responsive-img ag-logo-main" src="http://34.94.0.6/wp-content/uploads/2019/08/ag-logo-transparent.png"></img>
