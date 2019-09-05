398d1263e0673a37baef0bf942352c0343a28702
cwallace <devcwallace@gmail.com>
2019-09-05 15:33:02 -0500

    mobile nav btn

--- header.php
+++ header.php
@@ -29,14 +29,14 @@
 			<div class="col s12">
 				<img class="responsive-img rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens"></img>
 			</div>
-			<a href="#" data-activates="mobile-demo" class="button-collapse fixed right col s1"><i class="material-icons black-text main-mobile-menu-btn">menu</i></a>
+			<nav>
+				<a href="#" data-activates="mobile-demo" class="button-collapse fixed right col s2"><i class="material-icons black-text main-mobile-menu-btn">menu</i></a>
+			</nav>
+
 			<div class="col s6 push-s3">
-
-					<a href="/home">
-						<img class="responsive-img ag-logo-main" src="http://34.94.0.6/wp-content/uploads/2019/08/ag-logo-transparent.png"></img>
-					</a>
-
-
+				<a href="/home">
+					<img class="responsive-img ag-logo-main" src="http://34.94.0.6/wp-content/uploads/2019/08/ag-logo-transparent.png"></img>
+				</a>
 			</div>
 		</div>
 
