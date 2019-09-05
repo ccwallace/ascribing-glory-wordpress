67f7b982cf3d83bbad8b2fade6ae346d54a7d8ed
cwallace <devcwallace@gmail.com>
2019-09-05 16:09:43 -0500

    add cart

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 				<img class="responsive-img rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens"></img>
 			</div>
 			<nav class="transparent z-depth-0">
-				<a href="#" data-activates="mobile-demo" class="button-collapse fixed right col s1"><i class="material-icons black-text main-mobile-menu-btn">menu</i></a>
+				<a href="#" data-activates="mobile-demo" class="button-collapse fixed right col s2"><i class="material-icons black-text main-mobile-menu-btn">menu</i></a>
 			</nav>
 			<div class="col s6 push-s3">
 				<a href="/home">
@@ -51,6 +51,7 @@
             <li><a class="black-text" href="/resources">Resources</a></li>
             <li><a class="black-text" href="/shop">Shop</a></li>
             <li><a class="black-text" href="/contact">Contact</a></li>
+						<li><a class="black-text material-icons" href="/contact">shopping_cart</a></li>
           </ul>
         </div>
         <ul class="side-nav right-aligned" id="mobile-demo">
