3d4e6561f87dd981de53174eb1360d3794c6da5b
cwallace <devcwallace@gmail.com>
2019-09-05 14:59:42 -0500

    Update header.php

--- header.php
+++ header.php
@@ -30,12 +30,12 @@
 				<img class="responsive-img rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens"></img>
 			</div>
 			<div class="col s6 push-s3">
-				<div class="row">
+
 					<a href="/home">
 						<img class="responsive-img ag-logo-main" src="http://34.94.0.6/wp-content/uploads/2019/08/ag-logo-transparent.png"></img>
 					</a>
 					<a href="#" data-activates="mobile-demo" class="button-collapse fixed right col s1 push-s7"><i class="material-icons black-text">menu</i></a>
-				</div>
+				
 			</div>
 		</div>
 
