90dc6a86e12e60ed20871a98db89607dfae8d24f
cwallace <devcwallace@gmail.com>
2019-09-05 15:43:16 -0500

    nav stuff

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 			<div class="col s12">
 				<img class="responsive-img rainbow-pens" src="http://34.94.0.6/wp-content/uploads/2019/08/rainbow-pens-doubled.png" alt="Rainbow Pens"></img>
 			</div>
-			<nav class="transparent">
+			<nav class="transparent z-depth-0">
 				<a href="#" data-activates="mobile-demo" class="button-collapse fixed right col s2"><i class="material-icons black-text main-mobile-menu-btn">menu</i></a>
 			</nav>
 
@@ -44,7 +44,7 @@
 
 		<!--BEGIN NAVIGATION -->
 		<nav class="z-depth-0">
-      <div class="nav-wrapper white">
+      <div class="nav-wrapper transparent">
         <!--<a href="#" data-activates="mobile-demo" class="button-collapse fixed right"><i class="material-icons black-text">menu</i></a>-->
         <div class="nav-center">
           <ul class="hide-on-med-and-down">
