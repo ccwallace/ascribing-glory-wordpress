d7d0eaae04e24351db7c5bfbf6f074232a0ce43b
cwallace <devcwallace@gmail.com>
2019-09-05 21:29:44 -0500

    main page addition

--- front-page.php
+++ front-page.php
@@ -6,13 +6,18 @@ get_header();
       <div class="col s12 m7">
         <div class="card">
           <div class="card-image">
-            
+            <img src="img/pens-brushes.jpg" alt="">
+            <span class="card-title">Hand Holding Pens</span>
+          </div>
+          <div class="card-content">
+            <p>Simple Card description</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-
 <?php
  get_footer();
