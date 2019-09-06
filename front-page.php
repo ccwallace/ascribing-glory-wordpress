ae6f422c908218bbf387f11fb0e000288c89dfc3
cwallace <devcwallace@gmail.com>
2019-09-05 21:13:34 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,7 +1,17 @@
 <?php
 get_header();
  ?>
-
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m7">
+        <div class="card">
+          <div class="card-image">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 <?php
