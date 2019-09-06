22ab1da1a51b4628a37419ddbf3f6095d827aadf
cwallace <devcwallace@gmail.com>
2019-09-05 21:39:13 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@ get_header();
  ?>
   <div class="container">
     <div class="row">
-      <div class="col s12 m4">
+      <div class="col s4">
         <div class="card">
           <div class="card-image">
             <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes.jpg" alt="">
@@ -16,28 +16,34 @@ get_header();
             <a href="#">This is a link</a>
           </div>
         </div>
-        <div class="card-image">
-          <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes.jpg" alt="">
-          <span class="card-title">Hand Holding Pens</span>
+      </div>
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes.jpg" alt="">
+            <span class="card-title">Hand Holding Pens</span>
+          </div>
+          <div class="card-content">
+            <p>Simple Card description</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
         </div>
-        <div class="card-content">
-          <p>Simple Card description</p>
+      </div>
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes.jpg" alt="">
+            <span class="card-title">Hand Holding Pens</span>
+          </div>
+          <div class="card-content">
+            <p>Simple Card description</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
         </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-      <div class="card-image">
-        <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes.jpg" alt="">
-        <span class="card-title">Hand Holding Pens</span>
-      </div>
-      <div class="card-content">
-        <p>Simple Card description</p>
-      </div>
-      <div class="card-action">
-        <a href="#">This is a link</a>
-      </div>
-    </div>
       </div>
     </div>
   </div>
