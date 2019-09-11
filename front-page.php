07570f3941fec5c6eac895bfb7a28385fc0613c8
cwallace <devcwallace@gmail.com>
2019-09-11 10:30:20 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -39,7 +39,7 @@ get_header();
     </div>
 
     <div class="row">
-      <div class="col s10">
+      <div class="col s9">
           <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes-square.jpg" alt="" class="responsive-img image-test1 z-depth-2">
           <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes-square.jpg" alt="" class="responsive-img image-test2 z-depth-3">
           <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes-square.jpg" alt="" class="responsive-img image-test3 z-depth-2">
@@ -50,8 +50,8 @@ get_header();
           <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes-square.jpg" alt="" class="responsive-img image-test8 z-depth-3">
           <img src="<?php echo get_template_directory_uri();?>/img/pens-brushes-square.jpg" alt="" class="responsive-img image-test9 z-depth-2">
       </div>
-      <div class="col s2">
-        <div class="valign-wrapper">
+      <div class="valign-wrapper">
+        <div class="col s3">
           <h5>LOVE IS MEANT TO BE AN ADVENTURE</h5>
           <a class="waves-effect waves-light btn">Let's Tell Your Story</a>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
