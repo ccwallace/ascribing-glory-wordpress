54549036f40b43d81a8a13fdd57e3ad272627911
cwallace <devcwallace@gmail.com>
2019-09-12 11:33:28 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -70,6 +70,11 @@ get_header();
   <div class="col s4 center-align"><h5>Come Say Hi On Instagram</h5></div>
   <div class="divider col s4"></div>
 </div>
+<div class="row">
+  <div class="col s12 valign-wrapper center-align">
+    <a href="#"><p>Or Follow Along</p></a>
+  </div>
+</div>
 
   <div class="row instagram-row">
     <?php echo do_shortcode('[fts_instagram instagram_id=4365244084 pics_count=6 type=user profile_wrap=no super_gallery=yes columns=3 force_columns=no space_between_photos=1px icon_size=65px hide_date_likes_comments=no]'); ?>
