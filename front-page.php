4ef32cfc3f164a5d26c49a3629fea88d44d40d36
cwallace <devcwallace@gmail.com>
2019-09-13 16:13:23 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -75,16 +75,19 @@ get_header();
   <?php echo do_shortcode('[fts_instagram instagram_id=4365244084 pics_count=6 type=user profile_wrap=no super_gallery=yes columns=3 force_columns=yes space_between_photos=10px icon_size=65px hide_date_likes_comments=no]'); ?>
 </div>
 
-<div class="row">
-  <div class="col l12 s12">
-    <h3 class="center-align"><a href="#" class="responsive-text"></a>JOIN THE AG FAM!</h3>
+<div class="">
+  <div class="row">
+    <div class="col l12 s12">
+      <h4 class="center-align"><a href="#" class="responsive-text"></a>JOIN THE AG FAM!</h4>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col l12 s12">
+      <h5 class="center-align">Get early access + exclusive discounts for new collections, worksops, etc.</h5>
+    </div>
   </div>
 </div>
-<div class="row">
-  <div class="col l12 s12">
-    <h4 class="center-align">Get early access + exclusive discounts for new collections, worksops, etc.</h4>
-  </div>
-</div>
+
 
 <div class="row">
   <form class="col s12">
