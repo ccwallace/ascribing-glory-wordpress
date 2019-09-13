c11bb7a2eefa532cffffe0883f83b9a78002dbb2
cwallace <devcwallace@gmail.com>
2019-09-13 15:54:31 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,7 +14,23 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		
+		<!--BEGIN FOOTER NAVIGATION -->
+		<nav class="z-depth-0 transparent">
+			<div class="nav-wrapper">
+				<!--<a href="#" data-activates="mobile-demo" class="button-collapse fixed right"><i class="material-icons black-text">menu</i></a>-->
+				<div class="nav-center">
+					<ul class="hide-on-med-and-down">
+						<li><a class="black-text" href="/home">Home</a></li>
+						<li><a class="black-text" href="/about">About</a></li>
+						<li><a class="black-text" href="/weddings">Weddings</a></li>
+						<li><a class="black-text" href="/resources">Resources</a></li>
+						<li><a class="black-text" href="/shop">Shop</a></li>
+						<li><a class="black-text" href="/contact">Contact</a></li>
+						<li><a class="black-text material-icons" href="/cart">shopping_cart</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
