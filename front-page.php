79248c49a1af6e5c150769c04546f9da78b98071
cwallace <devcwallace@gmail.com>
2019-09-14 11:40:19 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -109,7 +109,10 @@ get_header();
 
 
 <div class="row">
-  <?php echo do_shortcode('[formidable id=2 title=false description=false]'); ?> 
+  <form class="col s12"></form>
+  <div class="row">
+    <?php echo do_shortcode('[formidable id=2 title=false description=false]'); ?>
+  </div>
 </div>
 
 
