fd60b37bbda3a14101a96b4b1d905b8bb1f4bd2e
cwallace <devcwallace@gmail.com>
2019-09-14 21:06:12 -0500

    Update archive-product.php

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -39,59 +39,57 @@ do_action( 'woocommerce_before_main_content' );
 		<div class="nav-wrapper">
 			<div class="nav-center">
 				<ul class="hide-on-med-and-down">
-					<li><a class="black-text" href="/home">Home</a></li>
+
+					<?php
+
+					$taxonomy     = 'product_cat';
+					$orderby      = 'name';
+					$show_count   = 0;      // 1 for yes, 0 for no
+					$pad_counts   = 0;      // 1 for yes, 0 for no
+					$hierarchical = 0;      // 1 for yes, 0 for no
+					$title        = '';
+					$empty        = 0;
+
+					$args = array(
+						'taxonomy'     => $taxonomy,
+						'orderby'      => $orderby,
+						'show_count'   => $show_count,
+						'pad_counts'   => $pad_counts,
+						'hierarchical' => $hierarchical,
+						'title_li'     => $title,
+						'hide_empty'   => $empty
+					);
+					$all_categories = get_categories( $args );
+					foreach ($all_categories as $cat) {
+						if($cat->category_parent == 0) {
+							$category_id = $cat->term_id;
+							echo '<li><a href="'. get_term_link($cat->slug, 'product_cat') .'" class="black-text">'. $cat->name .'</a></li>';
+
+							$args2 = array(
+								'taxonomy'     => $taxonomy,
+								'child_of'     => 0,
+								'parent'       => $category_id,
+								'orderby'      => $orderby,
+								'show_count'   => $show_count,
+								'pad_counts'   => $pad_counts,
+								'hierarchical' => $hierarchical,
+								'title_li'     => $title,
+								'hide_empty'   => $empty
+							);
+							$sub_cats = get_categories( $args2 );
+							if($sub_cats) {
+								foreach($sub_cats as $sub_category) {
+									echo  $sub_category->name ;
+								}
+							}
+						}
+					}
+
+					?>
 				</ul>
 			</div>
 		</div>
 	</nav>
-
-<?php
-
-  $taxonomy     = 'product_cat';
-  $orderby      = 'name';
-  $show_count   = 0;      // 1 for yes, 0 for no
-  $pad_counts   = 0;      // 1 for yes, 0 for no
-  $hierarchical = 0;      // 1 for yes, 0 for no
-  $title        = '';
-  $empty        = 0;
-
-  $args = array(
-         'taxonomy'     => $taxonomy,
-         'orderby'      => $orderby,
-         'show_count'   => $show_count,
-         'pad_counts'   => $pad_counts,
-         'hierarchical' => $hierarchical,
-         'title_li'     => $title,
-         'hide_empty'   => $empty
-  );
- $all_categories = get_categories( $args );
- foreach ($all_categories as $cat) {
-    if($cat->category_parent == 0) {
-        $category_id = $cat->term_id;
-        echo '<br /><a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
-
-        $args2 = array(
-                'taxonomy'     => $taxonomy,
-                'child_of'     => 0,
-                'parent'       => $category_id,
-                'orderby'      => $orderby,
-                'show_count'   => $show_count,
-                'pad_counts'   => $pad_counts,
-                'hierarchical' => $hierarchical,
-                'title_li'     => $title,
-                'hide_empty'   => $empty
-        );
-        $sub_cats = get_categories( $args2 );
-        if($sub_cats) {
-            foreach($sub_cats as $sub_category) {
-                echo  $sub_category->name ;
-            }
-        }
-    }
-}
-
- ?>
-
 	<?php
 	/**
 	 * Hook: woocommerce_archive_description.
