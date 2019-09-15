d3f3c2ae2da4dd39338f929f2e967f6bf4c96921
cwallace <devcwallace@gmail.com>
2019-09-14 22:23:58 -0500

    Update header.php

--- header.php
+++ header.php
@@ -63,14 +63,16 @@
 						<li><a class="black-text material-icons" href="/cart">shopping_cart</a></li>
           </ul>
         </div>
-        <ul class="side-nav right-aligned" id="mobile-demo">
-          <li><a href="/home">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/weddings">Weddings</a></li>
-          <li><a href="/resources">Resources</a></li>
-          <li><a href="/shop">Shop</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
       </div>
     </nav>
+		<div class="nav-wrapper">
+			<ul class="side-nav right-aligned" id="mobile-demo">
+				<li><a href="/home">Home</a></li>
+				<li><a href="/about">About</a></li>
+				<li><a href="/weddings">Weddings</a></li>
+				<li><a href="/resources">Resources</a></li>
+				<li><a href="/shop">Shop</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
 	<div id="content" class="site-content">
