7e3fe8e7dd911abd902927c73c3fb47d89502278
cwallace <devcwallace@gmail.com>
2019-09-14 21:56:56 -0500

    Update archive-product.php

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -102,7 +102,7 @@ do_action( 'woocommerce_before_main_content' );
 	}
 	echo('
 	</ul>
-	<nav>
+	<nav class="z-depth-0 transparent">
 		<div class="nav-wrapper">
 		<ul class="center hide-on-large-only">
 			<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Filter<i class="material-icons right">arrow_drop_down</i></a></li>
