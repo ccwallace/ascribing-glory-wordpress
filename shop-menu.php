504e2a0bcc58124ed9080e69907541c343204aa6
cwallace <devcwallace@gmail.com>
2019-09-17 17:36:05 -0500

    Update shop-menu.php

--- shop-menu.php
+++ shop-menu.php
@@ -72,10 +72,8 @@ echo('</ul>');
 
 
 <?php
-$menu_name = 'Product-Categories';
-$locations = get_nav_menu_locations();
-$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-$menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
+
+$menuitems = wp_get_nav_menu_items( 'Product-Categories', array( 'order' => 'DESC' ) );
 ?>
 
 <nav>
