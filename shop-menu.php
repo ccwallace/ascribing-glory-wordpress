244a2ae9992100553dd80074125106b9118a905e
cwallace <devcwallace@gmail.com>
2019-09-17 17:58:50 -0500

    Update shop-menu.php

--- shop-menu.php
+++ shop-menu.php
@@ -13,47 +13,63 @@
         $count = 0;
         $submenu = false;
 
-        foreach( $menuitems as $item ):
-          // set up title and url
-          $title = $item->title;
-          $link = $item->url;
+  foreach( $menuitems as $item ):
+    // set up title and url
+    $title = $item->title;
+    $link = $item->url;
 
-          // item does not have a parent so menu_item_parent equals 0 (false)
-          if ( !$item->menu_item_parent ):
-
-            // save this id for later comparison with sub-menu items
-            $parent_id = $item->ID;
-            ?>
-            <li class="item">
-              <a href="<?php echo $link; ?>" class="black-text">
-                <?php echo $title; ?>
-              </a>
-            <?php endif; ?>
-
-            <?php if ( $parent_id == $item->menu_item_parent ): ?>
-
-              <?php if ( !$submenu ): $submenu = true; ?>
-                <ul class="sub-menu">
-                <?php endif; ?>
-
-
-                <li class="item">
-                  <a href="<?php echo $link; ?>" class="black-text"><?php echo $title; ?></a>
-                </li>
-
-                <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
-                </ul>
-                <?php $submenu = false; endif; ?>
-
-              <?php endif; ?>
-
-              <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id ): ?>
-              </li>
-              <?php $submenu = false; endif; ?>
-
-              <?php $count++; endforeach; ?>
-
-            </ul>
+    // item does not have a parent so menu_item_parent equals 0 (false)
+    if ( !$item->menu_item_parent ):
+      // save this id for later comparison with sub-menu items
+      $parent_id = $item->ID;
+      ?>
+      <li class="item"><a href="<?php echo $link; ?>" class="black-text"><?php echo $title; ?></a>
+      <?php endif; ?>
+      <?php if ( $parent_id == $item->menu_item_parent ): ?>
+        <?php if ( !$submenu ): $submenu = true; ?>
+          <ul>
+          <?php endif; ?>
+            <li class="item"><a href="<?php echo $link; ?>" class="black-text"><?php echo $title; ?></a></li>
+          <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
+          </ul>
+          <?php $submenu = false; endif; ?>
+        <?php endif; ?>
+        <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id ): ?>
+        </li>
+        <?php $submenu = false; endif; ?>
+        <?php $count++; endforeach; ?>
+      </ul>
     </div>
   </div>
 </nav>
+<ul id="dropdown1" class="dropdown-content">
+  <?php
+  $count = 0;
+  $submenu = false;
+
+foreach( $menuitems as $item ):
+// set up title and url
+$title = $item->title;
+$link = $item->url;
+
+// item does not have a parent so menu_item_parent equals 0 (false)
+if ( !$item->menu_item_parent ):
+// save this id for later comparison with sub-menu items
+$parent_id = $item->ID;
+?>
+<li class="item"><a href="<?php echo $link; ?>" class="black-text"><?php echo $title; ?></a>
+<?php endif; ?>
+<?php if ( $parent_id == $item->menu_item_parent ): ?>
+  <?php if ( !$submenu ): $submenu = true; ?>
+    <ul>
+    <?php endif; ?>
+      <li class="item"><a href="<?php echo $link; ?>" class="black-text"><?php echo $title; ?></a></li>
+    <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
+    </ul>
+    <?php $submenu = false; endif; ?>
+  <?php endif; ?>
+  <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id ): ?>
+  </li>
+  <?php $submenu = false; endif; ?>
+  <?php $count++; endforeach; ?>
+</ul>
