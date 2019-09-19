086dd5ad2f79a582165ba247bb7fc0fdb8c8084d
cwallace <devcwallace@gmail.com>
2019-09-18 19:12:00 -0500

    Update content-product.php

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -40,7 +40,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_thumbnail - 10
 	 */
 	do_action( 'woocommerce_before_shop_loop_item_title' );
-	echo "howdy!";
+	echo "<h1>howdy!</h1>";
 	/**
 	 * Hook: woocommerce_shop_loop_item_title.
 	 *
