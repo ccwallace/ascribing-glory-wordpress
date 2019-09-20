4bcf56f75ef1bfb6b7946276fd3ed241f7e8a650
cwallace <devcwallace@gmail.com>
2019-09-19 21:18:11 -0500

    Update description.php

--- woocommerce/single-product/tabs/description.php
+++ woocommerce/single-product/tabs/description.php
@@ -38,3 +38,8 @@ $heading = apply_filters( 'woocommerce_product_description_heading', __( 'Descri
 		<h5 class="center-align">Shipping & Returns</h5>
 	</div>
 </div>
+<div class="row">
+	<div class="col s10">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
