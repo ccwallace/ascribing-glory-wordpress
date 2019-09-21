de683ff7b015d80d2c462025d29ec90e2df757a5
cwallace <devcwallace@gmail.com>
2019-09-21 12:57:03 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -200,7 +200,7 @@ function ag_remove_product_tabs( $tabs ) {
 add_filter('woocommerce_product_add_to_cart_text', 'custom_woocommerce_product_add_to_cart_text');
 function custom_woocommerce_product_add_to_cart_text(){
 		global $product;
-		$product_type = $product->$product_type;
+		$product_type = $product->get_type();
 		switch($product_type){
 			case 'variable':
 				return __('View Options', 'woocommerce');
