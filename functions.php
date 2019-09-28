f32378cc12a661a3b608bfacd0c15587c4119de7
cwallace <devcwallace@gmail.com>
2019-09-28 18:44:56 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -299,3 +299,7 @@ function product_enquiry_custom_form() {
 		<?php
 		}
 	}
+
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
