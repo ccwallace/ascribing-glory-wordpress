c0129104aedc4c304f745acc8552cf23a85933f2
cwallace <devcwallace@gmail.com>
2019-09-28 16:52:02 -0500

    Update shop-menu.php

--- shop-menu.php
+++ shop-menu.php
@@ -29,7 +29,7 @@
         <?php if ( !$submenu ): $submenu = true; ?>
           <ul>
           <?php endif; ?>
-            <li class="item"><a href="<?php echo $link; ?>" class="black-text"><?php echo $title; ?></a></li>
+            <li class="item"><a href="<?php echo $link; ?>" class="black-text" style="text-transform: uppercase;"><?php echo $title; ?></a></li>
           <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
           </ul>
           <?php $submenu = false; endif; ?>
