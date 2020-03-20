8bdaf1f21b9ef6077cf46c5b0d3e5a3079908282
cwallace <devcwallace@gmail.com>
2020-03-19 19:06:54 -0500

    Update header.php

--- header.php
+++ header.php
@@ -79,7 +79,7 @@
             <li><a class="dropdown-button black-text" data-beloworigin="true" href="#!" data-activates="dropdown3">WEDDINGS<i class="material-icons right">arrow_drop_down</i></a></li>
             <li><a class="black-text" href="/workshops">WORKSHOPS</a></li>
 			<!-- <li><a class="black-text" href="/shop-main">SHOP</a></li> -->
-			<li><a class="dropdown-button black-text" data-beloworigin="true" href="#!" data-coverTrigger="true" data-activates="dropdown6">SHOP<i class="material-icons right">arrow_drop_down</i></a></li>
+			<li><a class="dropdown-button black-text" data-beloworigin="true" href="/shop-main" data-activates="dropdown6">SHOP<i class="material-icons right">arrow_drop_down</i></a></li>
 			<li><a class="black-text" href="/my-account">MY ACCOUNT</a></li>
             <li><a class="black-text" href="/contact">CONTACT</a></li>
 			<li><a class="black-text material-icons" href="/cart">shopping_cart</a></li>
