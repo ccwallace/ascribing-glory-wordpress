2b66698aa7d70ba6da7ce8e74d7983215b0b888d
CWallace2 <devcwallace@gmail.com>
2021-11-11 09:33:18 -0600

    revert previous two tests

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -28,4 +28,23 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 
  //this is where to work on the tabs issue
+$tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
+if ( ! empty( $tabs ) ) : ?>
+
+	<div class="woocommerce-tabs wc-tabs-wrapper">
+		<ul class="tabs wc-tabs" role="tablist" style="display: none;">
+			<?php foreach ( $tabs as $key => $tab ) : ?>
+				<li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+					<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+		<?php foreach ( $tabs as $key => $tab ) : ?>
+			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+				<?php if ( isset( $tab['callback'] ) ) { call_user_func( $tab['callback'], $key, $tab ); } ?>
+			</div>
+		<?php endforeach; ?>
+	</div>
+
+<?php endif; ?>
